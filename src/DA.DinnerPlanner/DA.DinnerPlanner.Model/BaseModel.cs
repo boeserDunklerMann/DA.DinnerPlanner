ff440f3235834230d6f6ee17b4cd9a2e58628e6e
André <andre.dunkel@t-online.de>
2024-12-16 11:17:01 +0100

    BaseModel.cs added

---
+++ src/DA.DinnerPlanner/DA.DinnerPlanner.Model/BaseModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA.DinnerPlanner.Model
+{
+	/// <ChangeLog>
+		/// <Create Datum="16.12.2024" Entwickler="DA" />
+		/// </ChangeLog>
+	public abstract class BaseModel
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Änderungsdatum
+		/// </summary>
+		public DateTime? ChangeDate { get; set; }
+
+		/// <summary>
+		/// Erstelldatum
+		/// </summary>
+		public DateTime? CreationDate { get; set; }
+		public bool Deleted { get; set; }
+		public static T Create<T>() where T : BaseModel, new()
+			=> new T { ChangeDate = DateTime.UtcNow, CreationDate = DateTime.UtcNow, Deleted = false };
+	}
+}
