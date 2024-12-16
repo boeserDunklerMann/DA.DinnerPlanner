9b39c10aacb559ea251107c9816182b4eafcbaa3
André <andre.dunkel@t-online.de>
2024-12-16 11:10:04 +0100

    DinnerPlannerContext.cs added

---
+++ src/DA.DinnerPlanner/DA.DinnerPlanner.Model/DinnerPlannerContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA.DinnerPlanner.Model
+{
+	/// <ChangeLog>
+	/// <Create Datum="16.12.2024" Entwickler="DA" />
+	/// </ChangeLog>
+	public class DinnerPlannerContext(string connectionString):DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			// https://stackoverflow.com/questions/74060289/mysqlconnection-open-system-invalidcastexception-object-cannot-be-cast-from-d
+			// MariaDB 11+ doesnt work because of nullable PKs?
+			optionsBuilder.UseMySQL(connectionString);	// MariaDB10
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			// TODO DA: add entities here
+		}
+	}
+}
