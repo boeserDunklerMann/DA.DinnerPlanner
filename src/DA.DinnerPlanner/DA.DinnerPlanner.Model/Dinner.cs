9be72ceab62d2f98d21a74376653d8be8cf112bc
André <andre.dunkel@t-online.de>
2024-12-16 14:57:17 +0100

    class Dinner added

---
+++ src/DA.DinnerPlanner/DA.DinnerPlanner.Model/Dinner.cs
@@ -0,0 +1,22 @@
+﻿namespace DA.DinnerPlanner.Model
+{
+	/// <ChangeLog>
+	/// <Create Datum="16.12.2024" Entwickler="DA" />
+	/// </ChangeLog>
+	/// <summary>
+	/// class for a (planned) dinner, containing the guests, cooks and host
+	/// </summary>
+	public class Dinner : BaseModel
+	{
+		public Dinner()
+		{
+		}
+
+		public User Host { get; set; } = new();
+
+		public ICollection<User> Cooks { get; set; } = [];
+		public ICollection<User> Guests { get; set; } = [];
+		public DateTime DinnerDate { get; set; }
+		public string Dinnerdescription { get; set; }
+	}
+}
