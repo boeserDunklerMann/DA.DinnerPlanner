6f67e241f890a2f6ce0e1af17405f813b19eacea
André <andre.dunkel@t-online.de>
2024-12-19 17:02:21 +0100

    DPLAN-12

---
+++ src/DA.DinnerPlanner/DA.DinnerPlanner.Razor.Proto/Pages/EditUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DA.DinnerPlanner.Razor.Proto.Pages
+{
+    public class EditUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
