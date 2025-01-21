94c0b1f0790b1b81b1e2d3d5567f3db04692d0ed
André <andre.dunkel@t-online.de>
2025-01-21 15:47:03 +0100

    DPLAN-26

--- src/DA.DinnerPlanner/DA.DinnerPlanner.Razor.Proto/Pages/EditCommunications.cshtml.cs
+++ src/DA.DinnerPlanner/DA.DinnerPlanner.Razor.Proto/Pages/EditCommunications.cshtml.cs
@@ -1,8 +1,11 @@
 using DA.DinnerPlanner.Model;
 using DA.DinnerPlanner.Model.Contracts;
+using DA.DinnerPlanner.Model.UnitsTypes;
 using Hangfire;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace DA.DinnerPlanner.Razor.Proto.Pages
 {
@@ -18,6 +21,7 @@ namespace DA.DinnerPlanner.Razor.Proto.Pages
 		public int CommID { get; set; } // will be set on delete
 
 		public ICollection<Communication>? Communications { get; set; }
+        public SelectList? CommTypesSL { get; set; }
 
         private User? editUser;
 		public async Task<IActionResult> OnGetAsync()
@@ -32,7 +36,9 @@ namespace DA.DinnerPlanner.Razor.Proto.Pages
             await LoadUserDataAsync();
             if (editUser == null)
                 return NotFound();
-            editUser.CommunicationList.First(c => c.Id == CommID).Deleted = true;
+            Communication comm = editUser.CommunicationList.First(c => c.Id == CommID);
+            comm.Deleted = true;
+            comm.CommunicationValue = comm.CommunicationValue ?? "";
             await db.SaveAsync();
             Communications!.First(c => c.Id == CommID).Deleted = true;
 			return Page();
@@ -40,13 +46,31 @@ namespace DA.DinnerPlanner.Razor.Proto.Pages
         public async Task<IActionResult> OnPostAddAsync(int userId)
         {
 			await LoadUserDataAsync();
+			if (editUser == null)
+				return NotFound();
+            editUser.CommunicationList.Add(new() { CommunicationType=await db.CommunicationTypes.FirstAsync()});
+            await db.SaveAsync();
+			await LoadUserDataAsync();
 			return Page();
 		}
-
-        private async Task LoadUserDataAsync()
+        public async Task<IActionResult> OnPostSubmitAsync(int commTypeId, string commValue, int userId, int commId)
+        {
+			await LoadUserDataAsync();
+			if (editUser == null)
+				return NotFound();
+            Communication comm = editUser.CommunicationList.First(c => c.Id == commId);
+            comm.CommunicationValue = commValue;
+            comm.CommunicationType = await db.CommunicationTypes.FirstAsync(ct => ct.Id == commTypeId);
+            await db.SaveAsync();
+            return Page();
+		}
+		private async Task LoadUserDataAsync()
         {
 			editUser = (await application.GetAllUsersAsync(db)).First(u => u.Id == UserID);
 			Communications = editUser.CommunicationList.Where(c=>!c.Deleted).ToList();
+
+            var commTypesQry = db.CommunicationTypes.OrderBy(ct => ct.Name);
+            CommTypesSL = new SelectList(commTypesQry, nameof(CommunicationType.Id), nameof(CommunicationType.Name));
 		}
 	}
 }
