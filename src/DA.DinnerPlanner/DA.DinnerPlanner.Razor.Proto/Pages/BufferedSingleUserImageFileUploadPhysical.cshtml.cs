05fbda18ccb3b6357631c205415e20b393d94814
André <andre.dunkel@t-online.de>
2025-02-13 10:25:35 +0100

    DPLAN-29 (store imgs to DB)
    warnings removed

--- src/DA.DinnerPlanner/DA.DinnerPlanner.Razor.Proto/Pages/BufferedSingleUserImageFileUploadPhysical.cshtml.cs
+++ src/DA.DinnerPlanner/DA.DinnerPlanner.Razor.Proto/Pages/BufferedSingleUserImageFileUploadPhysical.cshtml.cs
@@ -1,52 +1,71 @@
 using DA.DinnerPlanner.Common;
+using DA.DinnerPlanner.Model;
 using DA.DinnerPlanner.Model.Contracts;
+using DA.DinnerPlanner.Model.UnitsTypes;
 using Hangfire;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel.DataAnnotations;
-using static Org.BouncyCastle.Math.EC.ECCurve;
 
 namespace DA.DinnerPlanner.Razor.Proto.Pages
 {
-    /// <ChangeLog>
-    /// <Create Datum="11.02.2025" Entwickler="DA" />
-    /// </ChangeLog>
-    public class BufferedSingleUserImageFileUploadPhysicalModel
+	/// <ChangeLog>
+	/// <Create Datum="11.02.2025" Entwickler="DA" />
+	/// <Change Datum="13.02.2025" Entwickler="DA">User loading stuff added (Jira-Nr. DPLAN-29)</Change>
+	/// </ChangeLog>
+	public class BufferedSingleUserImageFileUploadPhysicalModel
         :BasePageModel
     {
         private readonly long fileSizeLimitBytes;
-        private readonly string[] permittedExtensions = { "*.jpg", "*.png" };
-        private readonly string targetFilePath;
+        private readonly string[] permittedExtensions = { ".jpg", ".png" };
 		public BufferedSingleUserImageFileUploadPhysicalModel(IConfiguration config, IBackgroundJobClient backgroundJob, IDinnerPlannerContext context)
 			: base(config, backgroundJob, context)
 		{
             fileSizeLimitBytes = config.GetValue<long>("UserProfileImage:FileSizeLimit");
-            targetFilePath = config.GetValue<string>("UserProfileImage:StoredFilesPath")!;
+            //targetFilePath = config.GetValue<string>("UserProfileImage:StoredFilesPath")!;
 		}
 
         [BindProperty]
-        public BufferedSingleFileUploadPhysical FileUpload { get; set; }
-        public string Result { get; private set; }
+        public required BufferedSingleFileUploadPhysical FileUpload { get; set; }
+        public string? Result { get; private set; }
+		
+        [BindProperty(SupportsGet = true)]
+		public int? UserID { get; set; }
+		public User? EditUser { get; private set; }
+		public async Task<IActionResult> OnGetAsync()
+		{
+			if (UserID == null)
+				return NotFound();
+			await LoadUserDataAsync();
+			return Page();
+		}
 
-        public async Task<IActionResult> OnPostUploadAsync()
+		public async Task<IActionResult> OnPostUploadAsync()
         {
             if (!ModelState.IsValid)
             {
                 Result = "Please correct the form!";
                 return Page();
             }
-            
+			if (FileUpload == null || FileUpload.FormFile == null)
+			{
+				Result = $"internal error. {nameof(FileUpload)} is null.";
+				return BadRequest();
+			}
+
             var formFileContent = await FileHelpers.ProcessFormFile<BufferedSingleFileUploadPhysical>(FileUpload.FormFile,
                 ModelState, permittedExtensions, fileSizeLimitBytes);
 		
             if (!ModelState.IsValid)
 			{
-				Result = "Please correct the form!";
+                Result = "";
 				return Page();
 			}
 
-            string trustedFileNameForStorage = Path.GetRandomFileName();
-            string filePath = Path.Combine(targetFilePath, trustedFileNameForStorage);
+			await LoadUserDataAsync();
+			if (EditUser == null)
+				return NotFound();
+
 
 			// **WARNING!**
 			// In the following example, the file is saved without
@@ -56,24 +75,31 @@ namespace DA.DinnerPlanner.Razor.Proto.Pages
 			// for download or for use by other systems. 
 			// For more information, see the topic that accompanies 
 			// this sample.
-			using var fileStream = System.IO.File.Create(filePath);
-			await fileStream.WriteAsync(formFileContent);
+			//using var fileStream = System.IO.File.Create(filePath);
+			//await fileStream.WriteAsync(formFileContent);
 
 			// To work directly with a FormFile, use the following
 			// instead:
 			//await FileUpload.FormFile.CopyToAsync(fileStream);
-            return RedirectToPage("Index");
+			
+			EditUser.UserImages.Add(new UserImage() { Image = formFileContent });
+			await db.SaveAsync();
+			return RedirectToPage("Index");
+		}
+		private async Task LoadUserDataAsync()
+		{
+			EditUser = (await application.GetAllUsersAsync(db)).First(u => u.Id == UserID);
 		}
 	}
 
-    /// <ChangeLog>
-    /// <Create Datum="11.02.2025" Entwickler="DA" />
-    /// </ChangeLog>
-    public class BufferedSingleFileUploadPhysical
+	/// <ChangeLog>
+	/// <Create Datum="11.02.2025" Entwickler="DA" />
+	/// </ChangeLog>
+	public class BufferedSingleFileUploadPhysical
     {
         [Required]
         [Display(Name = "File")]
-        public IFormFile FormFile { get; set; }
+        public IFormFile? FormFile { get; set; }
 
         [Display(Name = "Note")]
         [StringLength(50, MinimumLength = 0)]
