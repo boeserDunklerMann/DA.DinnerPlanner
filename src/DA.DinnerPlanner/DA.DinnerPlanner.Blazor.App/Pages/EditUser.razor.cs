f1207bc34ed0c4f4308039ba64dfa81c8a3ae453
André <andre.dunkel@t-online.de>
2025-02-27 14:02:37 +0100

    DPLAN-54

---
+++ src/DA.DinnerPlanner/DA.DinnerPlanner.Blazor.App/Pages/EditUser.razor.cs
@@ -0,0 +1,32 @@
+﻿using DA.DinnerPlanner.Common;
+using DA.DinnerPlanner.Model;
+using DA.DinnerPlanner.Model.UnitsTypes;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace DA.DinnerPlanner.Blazor.App.Pages
+{
+	/// <ChangeLog>
+	/// <Create Datum="27.02.2025" Entwickler="DA" />
+	/// </ChangeLog>
+	public partial class EditUser : ComponentBase
+	{
+		[Parameter]
+		public int UserID { get; set; }
+		public User? EditingUser { get; set; }
+		private readonly Application application = Application.Instance;
+		protected override async Task OnInitializedAsync()
+		{
+			if (UserID > 0)
+			{
+				EditingUser = (await application.GetAllUsersAsync(dpcontext)).First(u => u.Id == UserID);
+			}
+			await base.OnInitializedAsync();
+		}
+
+		private async Task OnEditUserSubmitAsync()
+		{
+			await dpcontext.SaveAsync();
+		}
+	}
+}
