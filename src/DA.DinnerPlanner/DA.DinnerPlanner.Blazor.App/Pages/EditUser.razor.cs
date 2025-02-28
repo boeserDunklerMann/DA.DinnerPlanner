bfd8f5c39ee78d57eb512958e5ada50515750faf
André <andre.dunkel@t-online.de>
2025-02-28 10:25:53 +0100

    DPLAN-54

--- src/DA.DinnerPlanner/DA.DinnerPlanner.Blazor.App/Pages/EditUser.razor.cs
+++ src/DA.DinnerPlanner/DA.DinnerPlanner.Blazor.App/Pages/EditUser.razor.cs
@@ -1,6 +1,5 @@
 ﻿using DA.DinnerPlanner.Common;
 using DA.DinnerPlanner.Model;
-using DA.DinnerPlanner.Model.UnitsTypes;
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +7,7 @@ namespace DA.DinnerPlanner.Blazor.App.Pages
 {
 	/// <ChangeLog>
 	/// <Create Datum="27.02.2025" Entwickler="DA" />
+	/// <Change Datum="28.02.2025" Entwickler="DA">Carrier classes added for Checkboxes and select list</Change>
 	/// </ChangeLog>
 	public partial class EditUser : ComponentBase
 	{
@@ -20,13 +20,76 @@ namespace DA.DinnerPlanner.Blazor.App.Pages
 			if (UserID > 0)
 			{
 				EditingUser = (await application.GetAllUsersAsync(dpcontext)).First(u => u.Id == UserID);
+				await LoadCheckboxesAsync();
 			}
 			await base.OnInitializedAsync();
 		}
 
-		private async Task OnEditUserSubmitAsync()
+		private ICollection<AllergyCarrier> Allergies { get; set; } = [];
+		private ICollection<LanguageCarrier> Languages { get; set; } = [];
+		private ICollection<PetCarrier> Pets { get; set; } = [];
+		private async Task LoadCheckboxesAsync()
 		{
-			await dpcontext.SaveAsync();
+			await dpcontext.Allergies.ForEachAsync(a => Allergies.Add(new() { Allergy = a, Selected = EditingUser!.Allergies.Contains(a) }));
+			await dpcontext.Languages.ForEachAsync(l => Languages.Add(new() { Language = l, Selected = EditingUser!.Languages.Contains(l) }));
+			await dpcontext.Pets.ForEachAsync(p => Pets.Add(new() { Pet = p, Selected = EditingUser!.Pets.Contains(p) }));
 		}
+
+		#region checkbox-toggles
+		private async Task ToggleAllergyAsync(ChangeEventArgs args, AllergyCarrier allergy)
+		{
+			if ((bool)args.Value!)
+				EditingUser!.Allergies.Add(allergy.Allergy!);
+			else
+				EditingUser!.Allergies.Remove(allergy.Allergy!);
+
+			await Task.CompletedTask;
+		}
+		private async Task ToggleLanguageAsync(ChangeEventArgs args, LanguageCarrier language)
+		{
+			if ((bool)args.Value!)
+				EditingUser!.Languages.Add(language.Language!);
+			else
+				EditingUser!.Languages.Remove(language.Language!);
+
+			await Task.CompletedTask;
+		}
+		private async Task TogglePetAsync(ChangeEventArgs args, PetCarrier pet)
+		{
+			if ((bool)args.Value!)
+				EditingUser!.Pets.Add(pet.Pet!);
+			else
+				EditingUser!.Pets.Remove(pet.Pet!);
+
+			await Task.CompletedTask;
+		}
+		#endregion
+
+		public bool HideSavedLabel { get; set; } = true;
+
+		private void OnEditUserSubmit()
+		{
+			dpcontext.SaveChanges();
+			HideSavedLabel = false;
+		}
+
+		#region Carrier classes
+		internal class AllergyCarrier
+		{
+			public bool Selected { get; set; }
+			public Model.UnitsTypes.Allergy? Allergy { get; set; }
+		}
+		private class PetCarrier
+		{
+			public bool Selected { get; set; }
+			public Model.Pet? Pet { get; set; }
+		}
+		private class LanguageCarrier
+		{
+			public bool Selected { get; set; }
+			public Model.UnitsTypes.Language? Language { get; set; }
+		}
+
+		#endregion
 	}
 }
