8170fd409d66801d514ca03faef3b6c686e8e417
André <andre.dunkel@t-online.de>
2025-03-27 16:57:21 +0100

    DPLAN-80

--- src/DA.DinnerPlanner/DA.DinnerPlanner.Blazor.App/Pages/NotificationList.razor.cs
+++ src/DA.DinnerPlanner/DA.DinnerPlanner.Blazor.App/Pages/NotificationList.razor.cs
@@ -6,24 +6,73 @@ namespace DA.DinnerPlanner.Blazor.App.Pages
 {
 	/// <ChangeLog>
 	/// <Create Datum="20.03.2025" Entwickler="DA" />
+	/// <Change Datum="27.03.2025" Entwickler="DA">see https://learn.microsoft.com/de-de/aspnet/core/blazor/blazor-ef-core?view=aspnetcore-9.0#scope-a-database-context-to-the-lifetime-of-the-component (Jira-Nr. DPLAN-80)</Change>
 	/// </ChangeLog>
 	public partial class NotificationList : ComponentBase
 	{
 		public ICollection<Notification> Notifications { get; set; } = [];
+		private DinnerPlannerContext? dpcontext;
+		/// <summary>
+		/// Identifies whether a db-action is currently in progress
+		/// </summary>
+		private bool Loading { get; set; } = false;
 
 		protected override async Task OnInitializedAsync()
 		{
-			User ich = dpcontext.Users.First(u => u.Id == 1);
-			Notifications = [.. ich.Notifications.Where(n=>!n.Deleted)];
-			await Task.CompletedTask;
+			if (dpcontext == null)
+			{
+				dpcontext = await contextFactory.CreateDbContextAsync();
+				dpcontext.ConnectionString = configuration.GetConnectionString("da_dinnerplanner-db")!;
+			}
+			if (Loading)
+				return;
+			try
+			{
+				Loading = true;
+				User ich = dpcontext.Users.First(u => u.Id == 1);
+				Notifications = [.. ich.Notifications.Where(n => !n.Deleted)];
+				await Task.CompletedTask;
+			}
+			finally
+			{
+				Loading = false;
+			}
 		}
 
 		private async Task OnDeleteNotificationAsync(Notification notification)
 		{
-			notification.Delete();
-			await dpcontext.SaveAsync();
-			navMgr.NavigateTo(nameof(NotificationList), true);
-			await Task.CompletedTask;
+			if (Loading)
+				return;
+			try
+			{
+				Loading = true;
+				notification.Delete();
+				await dpcontext!.SaveAsync();
+				navMgr.NavigateTo(nameof(NotificationList), true);
+			}
+			finally
+			{
+				Loading = false;
+			}
 		}
+
+		#region Disposing
+		// see: https://learn.microsoft.com/de-de/dotnet/fundamentals/code-analysis/quality-rules/ca1816#example-that-satisfies-ca1816
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				dpcontext?.Dispose();
+				dpcontext = null;
+			}
+		}
+		#endregion
 	}
 }
